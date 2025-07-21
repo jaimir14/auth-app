499ba325e8e29929fd8564675a532b8d58bbc8b5
jairo <jaimir14@gmail.com>
2025-07-21 12:02:00 -0600

    Added cors for domain

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [
-      'http://localhost:8081', // React
-    ]
+      'http://localhost:8081', // React development
+      /^https:\/\/.*\.tiempos\.website$/, // All subdomains of tiempos.website
+    ],
+    credentials: true, // Allow credentials if needed
   });
   await app.listen(process.env.PORT ?? 3000);
 }
